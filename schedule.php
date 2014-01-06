6e5d066835515c507ce7bc302c5ecda64e36c47f
François LASSERRE <choiz@me.com>
2014-01-06 19:32:25 +0100

    Add exit if rules are too high

--- schedule.php
+++ schedule.php
@@ -3,7 +3,7 @@
 include 'function.php';
 
 $rules = array(
-    'artist_separation' => 4,
+    'artist_separation' => 2,
     'track_separation' => 6,
     'playlist_size' => 60
 );
@@ -14,6 +14,8 @@ $tracks = read_folder($folder);
 
 foreach($tracks as $track) {
     list($artist, $title) = explode(' - ', $track);
+    $artist_list[] = $artist;
+    $track_list[] = $title;
     $song = new stdclass;
     $song->artist = $artist;
     $song->title = $title;
@@ -21,17 +23,30 @@ foreach($tracks as $track) {
     $songs[] = $song;
 }
 
-$playlist = array();
-$i = 0;
-
 $bac = $songs;
 shuffle($bac);
 
+$list_artist = array_unique($artist_list);
+$list_track = array_unique($track_list);
+
+$playlist = array();
+$i = 0;
+
 do {
+
+    if ((count($list_artist)/2) < $rules['artist_separation']) {
+        echo "WARNING: artist_separation is to high: ".$rules['artist_separation']." use a value between 1 and ".floor(count($list_artist)/2)."\n";
+        exit;
+    }
+
+    if ((count($list_track)/2) < $rules['track_separation']) {
+        echo "WARNING: track_separation is to high: ".$rules['track_separation']." use a value between 1 and ".floor(count($list_track)/2)."\n";
+        exit;
+    }
 
     $track = get_track($i,$bac,$playlist);
 
-    if($track) {
+    if ($track) {
         $playlist[] = $track;
     }
 
