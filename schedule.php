0e4034429cf52da5e864236579204181653d94a0
François LASSERRE <choiz@me.com>
2016-04-20 21:20:29 +0200

    Remove function and add function to schedule.php Update code

--- schedule.php
+++ schedule.php
@@ -1,9 +1,7 @@
 <?php
-
-include_once 'function.php';
-include_once 'rules.php';
-
+define('DEBUG', false);
 $cr = "\n";
+
 if (DIRECTORY_SEPARATOR == "\\") {
     $cr = "\r\n";
 }
@@ -12,7 +10,11 @@ if (!empty($argv[1])) {
     $rules_file = $argv[1];
     if (file_exists($rules_file)) {
         include_once($rules_file);
+    } else {
+        include_once 'rules.php';
     }
+} else {
+    include_once 'rules.php';
 }
 
 $artist_list = array();
@@ -115,4 +117,50 @@ foreach ($playlist['filename'] as $item) {
 
 if (file_put_contents($rules['playlist']['path'], $m3u_content) !== FALSE) {
     echo 'Playlist '.$rules['playlist']['path'].' saved.'.$cr;
+}
+
+function read_folder($folder) {
+    if ($handle = opendir($folder)) {
+        $out = array();
+        while (false !== ($entry = readdir($handle))) {
+            if ($entry != "." && $entry != "..") {
+                $out[] = $entry;
+            }
+        }
+        closedir($handle);
+        return $out;
+    }
+    return false;
+}
+
+function get_track($i, $bac, $playlist) {
+
+    global $rules;
+
+    $j = $i % count($bac);
+    $artist_last_diff = array_slice($playlist['artist'], -$rules['separation']['artist'], $rules['separation']['artist']);
+    $track_last_diff = array_slice($playlist['title'], -$rules['separation']['track'], $rules['separation']['track']);
+
+    if (isset($bac[$j])) {
+        if (in_array(strtolower($bac[$j]['artist']), $artist_last_diff)) {
+            if (DEBUG) {
+                echo "WARNING: artist ".strtolower($bac[$j]['artist'])." already diff\n";
+            }
+            return false;
+        }
+        if (in_array(strtolower($bac[$j]['title']), $track_last_diff)) {
+            if (DEBUG) {
+                echo "WARNING: track ".strtolower($bac[$j]['title'])." already diff\n";
+            }
+            return false;
+        }
+        if (DEBUG) {
+            echo "Track Add ".$bac[$j]['title']."\n";
+            echo "Artist Add ".$bac[$j]['artist']."\n";
+        }
+        return $bac[$j];
+    }
+
+    return false;
+
 }
