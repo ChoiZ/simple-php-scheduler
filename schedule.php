65c278cc6361a9737344b98e77fb22aaae0c4cc1
François LASSERRE <choiz@me.com>
2016-04-21 13:45:59 +0200

    Update code for multistation

--- schedule.php
+++ schedule.php
@@ -8,117 +8,135 @@ if (DIRECTORY_SEPARATOR == "\\") {
 
 include_once 'config.php';
 
+$file_loaded = 0;
+
 if (!empty($argv[1])) {
-    $rules_file = $argv[1];
-    if (file_exists($rules_file)) {
-        include_once($rules_file);
-    } else {
-        include_once 'rules/separation.php';
+    array_shift($argv);
+    foreach ($argv as $conf) {
+        if (file_exists($conf.'.php')) {
+            include_once($conf.'.php');
+            $file_loaded++;
+        }
     }
-} else {
+}
+
+if ($file_loaded === 0) {
     include_once 'rules/separation.php';
 }
 
-$artist_list = array();
-$track_list = array();
-$bac = array();
-$playlist = array();
-$playlist['artist'] = array();
-$playlist['title'] = array();
-$playlist['filename'] = array();
-$playlist['duration'] = array();
+$multistation = false;
+$nb_station = count($stations);
 
-$folder = $config['music']['folder'];
+if ($nb_station > 0) {
 
-$tracks = read_folder($folder);
-
-foreach($tracks as $track) {
-    $path_track = pathinfo($track);
-
-    if (in_array($path_track['extension'], $config['music']['ext'])) {
-        list($artist, $title) = explode(' - ', $path_track['filename']);
-        $artist_list[] = strtolower($artist);
-        $track_list[] = strtolower($title);
-        $song = array();
-        $song['artist'] = $artist;
-        $song['title'] = $title;
-        $song['filename'] = $folder.$track;
-        $song['duration'] = rand(135,305);
-        $bac[] = $song;
-    }
-}
-
-shuffle($bac);
-
-$max_artist = floor(count(array_unique($artist_list))/2);
-$max_track = floor(count(array_unique($track_list)));
-
-echo "max artist : ".$max_artist."\n";
-echo "max track : ".$max_track."\n";
-echo "max bac : ".count($bac)."\n";
-
-$error = array();
-
-if (empty($config['playlist']['path'])) {
-    $error[] = 'WARNING: playlist_path must be set in config.php file.';
-}
-
-if (empty($config['playlist']['size'])) {
-    $error[] = 'WARNING: playlist_size must be set in config.php file.';
-}
-
-if (empty($rules['separation']['artist'])) {
-    $error[] = 'WARNING: artist_separation must be set in separation.php file.';
-}
-
-if (empty($rules['separation']['track'])) {
-    $error[] = 'WARNING: track_separation must be set in separation.php file.';
-}
-
-if ($max_artist < $rules['separation']['artist']) {
-    $error[] = 'WARNING: artist_separation is too high: '.$rules['separation']['artist'].' use a value between 1 and '.$max_artist;
-}
-
-if ($max_track < $rules['separation']['track']) {
-    $error[] = 'WARNING: track_separation is too high: '.$rules['separation']['track'].' use a value between 1 and '.$max_track;
-}
-
-if (count($error)>0) {
-    foreach($error as $msg) {
-        echo $msg.$cr;
-    }
-    exit();
-}
-
-$i=0;
-
-do {
-
-    $track = get_track($i, $bac, $playlist);
-
-    if ($track !== false) {
-        $playlist['artist'][] = strtolower($track['artist']);
-        $playlist['duration'][] = $track['duration'];
-        $playlist['filename'][] = $track['filename'];
-        $playlist['title'][] = strtolower($track['title']);
+    if ($nb_station > 1) {
+        $multistation = true;
     }
 
-    $i++;
-    if ($i % $max_track == 2) {
+    foreach ($stations as $station) {
+
+        $toolong = false;
+        $artist_list = array();
+        $track_list = array();
+        $bac = array();
+        $playlist = array();
+        $playlist['artist'] = array();
+        $playlist['title'] = array();
+        $playlist['filename'] = array();
+        $playlist['duration'] = array();
+        $folder = $config['music']['folder'].$station.'/';
+
+        $tracks = read_folder($folder);
+
+        foreach($tracks as $track) {
+            $path_track = pathinfo($track);
+
+            if (in_array($path_track['extension'], $config['music']['ext'])) {
+                list($artist, $title) = explode(' - ', $path_track['filename']);
+                $artist_list[] = strtolower($artist);
+                $track_list[] = strtolower($title);
+                $song = array();
+                $song['artist'] = $artist;
+                $song['title'] = $title;
+                $song['filename'] = $folder.$track;
+                $song['duration'] = rand(135,305);
+                $bac[] = $song;
+            }
+        }
+
         shuffle($bac);
+
+        $max_artist = floor(count(array_unique($artist_list)));
+        $max_track = floor(count(array_unique($track_list)));
+
+        echo "max artist : ".$max_artist."\n";
+        echo "max track : ".$max_track."\n";
+        echo "max bac : ".count($bac)."\n";
+
+        $error = array();
+
+        if (empty($config['playlist']['path'])) {
+            $error[] = 'WARNING: playlist_path must be set in config.php file.';
+        }
+
+        if (empty($config['playlist']['size'])) {
+            $error[] = 'WARNING: playlist_size must be set in config.php file.';
+        }
+
+        if (empty($rules['separation']['artist'])) {
+            $error[] = 'WARNING: artist_separation must be set in separation.php file.';
+        }
+
+        if (empty($rules['separation']['track'])) {
+            $error[] = 'WARNING: track_separation must be set in separation.php file.';
+        }
+
+        if ($max_artist < $rules['separation']['artist']) {
+            $error[] = 'WARNING: artist_separation is too high: '.$rules['separation']['artist'].' use a value between 1 and '.$max_artist;
+        }
+
+        if ($max_track < $rules['separation']['track']) {
+            $error[] = 'WARNING: track_separation is too high: '.$rules['separation']['track'].' use a value between 1 and '.$max_track;
+        }
+
+        if (count($error)>0) {
+            foreach($error as $msg) {
+                echo $msg.$cr;
+            }
+            exit();
+        }
+
+        $i=0;
+
+        do {
+
+            $track = get_track($i, $bac, $playlist);
+
+            if ($track !== false) {
+                $playlist['artist'][] = strtolower($track['artist']);
+                $playlist['duration'][] = $track['duration'];
+                $playlist['filename'][] = $track['filename'];
+                $playlist['title'][] = strtolower($track['title']);
+            }
+
+            $i++;
+            if ($i % $max_track == 2) {
+                shuffle($bac);
+            }
+
+        } while(count($playlist['artist']) < $config['playlist']['size']);
+
+        $m3u_content = '#EXTM3U'.$cr;
+
+        foreach ($playlist['filename'] as $item) {
+            //$m3u_content .= '#EXTINF:'.$item->duration.', '.$item->artist.' - '.$item->title.$cr;
+            $m3u_content .= $item.$cr;
+        }
+
+        if (file_put_contents($config['playlist']['path'].$station.'.m3u', $m3u_content) !== FALSE) {
+            echo 'Playlist '.$config['playlist']['path'].$station.'.m3u saved.'.$cr;
+        }
     }
-
-} while(count($playlist['artist']) < $config['playlist']['size']);
-
-$m3u_content = '#EXTM3U'.$cr;
-
-foreach ($playlist['filename'] as $item) {
-    //$m3u_content .= '#EXTINF:'.$item->duration.', '.$item->artist.' - '.$item->title.$cr;
-    $m3u_content .= $item.$cr;
-}
-
-if (file_put_contents($config['playlist']['path'], $m3u_content) !== FALSE) {
-    echo 'Playlist '.$config['playlist']['path'].' saved.'.$cr;
 }
 
 function read_folder($folder) {
